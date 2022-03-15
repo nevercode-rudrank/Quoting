8e8facd32f1775dac833592cf433572f12b68b9a
Rudrank Riyam <rudrank@nevercode.io>
2022-03-16 00:26:22 +0530

    Initial commit

---
+++ Shared/QuotyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotyApp.swift
+//  Shared
+//
+//  Created by Rudrank Riyam on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
